1f1139865a4a7aeb1c5747c3197b785d3344b8a3
mattiabonazza <mattia.bonazza@gmail.com>
2020-12-26 17:52:42 +0100

    Update bme280_graph.py

--- bme280_graph.py
+++ bme280_graph.py
@@ -1,3 +1,11 @@
 # This file extends the functionality of the bme280.py script including a real-time plot of the measuring variable.
+# This code has been created using the resources found here: 'https://learn.sparkfun.com/tutorials/graph-sensor-data-with-python-and-matplotlib/all#update-a-graph-in-real-time'
+
 
 import time
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import bme280 as bme
+
+
