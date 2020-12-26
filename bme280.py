f00f025d42552ce9ffa42854139c81b2452cd0c2
mattiabonazza <mattia.bonazza@gmail.com>
2020-12-26 14:45:16 +0100

    Update and rename BME280.py to bme280.py

---
+++ bme280.py
@@ -0,0 +1,2 @@
+# BME280.py contains the functions and the settings to communcate with the sensor. The file was authored from Matt Hawkins and we copied the script from 
+# wget https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/python/bme280.py
